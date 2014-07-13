df493d0197a6dad4f191c1b6bcbb48b123346a2c
MuzzakirRamlan <muzzakir.ramlan@yahoo.com>
2014-07-13 16:39:19 +0100

    Initial Commit

---
+++ svdintel2/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  svdintel2
+//
+//  Created by Mohammad Muz'zakir  on 13/07/2014.
+//  Copyright (c) 2014 Mohammad Muz'zakir . All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
